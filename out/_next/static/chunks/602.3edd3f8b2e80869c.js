a134d51fc4703910bdb6eadcec63daea37e3248e
Guru sakthivel <gurusakthivel.v2021ecea@sece.ac.in>
2023-09-19 20:57:19 +0530

    added out folder

---
+++ out/_next/static/chunks/602.3edd3f8b2e80869c.js
@@ -0,0 +1,27 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[602],{64602:function(e,o,r){r.r(o),r.d(o,{default:function(){return a}});var l=r(46046);r(2265),r(33700);let u=(0,l.f)(l.h)`
+    background-color: ${l.e.white};
+    border-color: ${l.e.beauBlue};
+    color: ${l.e.blue};
+
+    :hover {
+        background-color: ${l.k.beauBlue};
+        border-color: transparent;
+        color: ${l.e.blue};
+
+        svg {
+            color: ${l.e.blue};
+        }
+    }
+
+    :active {
+        box-shadow: 0px 0px 0px 2px ${l.e.blueDark};
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${l.e.paleCerulean};
+    }
+
+    svg {
+        color: ${l.e.blue};
+    }
+`,{ButtonOutlineStyled:c}={ButtonOutlineStyled:u},a=({...e})=>(0,l.i)(c,{...e})}}]);
